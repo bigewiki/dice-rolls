fdee3f00c22abf247d8f5336bb03e396df155fce
Edward M <edwardtmuniz@gmail.com>
2019-09-28 23:00:53 -0700

    now displaying dice images instead of numbers

--- history.php
+++ history.php
@@ -1,7 +1,7 @@
 <?php
-//add the dice roll or create the session if it doesn't exist
+//add the dice roll else create the session if it doesn't exist
     if ( isset ($_SESSION['pastRolls'])){
-        //if we're storing more than 10 rolls replace it
+        //if we're storing more than 10 rolls replace it else push to array
         if(count($_SESSION['pastRolls']) == 10){
             $_SESSION['pastRolls'][] = $diceRolls;
             array_shift($_SESSION['pastRolls']);
@@ -14,24 +14,25 @@
 ?>
 <br/><br/>
 <table>
-<tr>
-    <th>Roll 1</th>
-    <th>Roll 2</th>
-    <th>Roll 3</th>
-    <th>Roll 4</th>
-    <th>Roll 5</th>
-    <th>Roll 6</th>
-    <th>Total</th>
-</tr>
-<?php
-    for ( $i = 0; $i < count($_SESSION['pastRolls']); $i ++){
-        $rollTotal = 0;
-        foreach ($_SESSION['pastRolls'][$i] as $value) {
-            $rollTotal+=$value;
+    <tr>
+        <th>Roll 1</th>
+        <th>Roll 2</th>
+        <th>Roll 3</th>
+        <th>Roll 4</th>
+        <th>Roll 5</th>
+        <th>Roll 6</th>
+        <th>Total</th>
+    </tr>
+    <?php
+        //loop through past rolls and build table
+        for ( $i = 0; $i < count($_SESSION['pastRolls']); $i ++){
+            $rollTotal = 0;
+            echo "<tr>";
+            foreach ($_SESSION['pastRolls'][$i] as $value) {
+                $rollTotal+=$value;
+                echo "<td><i class='fas fa-$diceIconArray[$value]'></i></td>";
+            }
+            echo "<td>$rollTotal</td></tr>";
         }
-        echo "<tr><td>";
-          echo implode("</td><td>",$_SESSION['pastRolls'][$i]);
-        echo "</td><td>$rollTotal</td></tr>";
-    }
-    echo "</table>";
-?>
+    ?>
+</table>
